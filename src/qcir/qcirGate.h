75aa414c34bf5ac8b4c3a81bffbf3b22682c0280
chinyi0523 <james31423a@gmail.com>
2022-08-22 04:33:00 +0800

    :sparkles: Add qcirp -Q (print qubit line)

--- src/qcir/qcirGate.h
+++ src/qcir/qcirGate.h
@@ -15,23 +15,49 @@
 
 using namespace std;
 
-// TODO: Feel free to define your own classes, variables, or functions.
-
 class QCirGate;
-
+struct BitInfo;
 //------------------------------------------------------------------------
 //   Define classes
 //------------------------------------------------------------------------
+struct BitInfo
+{
+  size_t _qubit;
+  QCirGate *_parent;
+  QCirGate *_child;
+  bool _isTarget;
+};
+
 class QCirGate
 {
 public:
-  // size_t id, string type, vector<size_t> pins
-  QCirGate(size_t id, string type, vector<size_t> pins) : _id(id), _type(type), _pins(pins) {}
+  QCirGate(size_t id, string type, size_t nqubit) : _id(id), _type(type), _nqubit(nqubit)
+  {
+    _qubits.clear();
+  }
   ~QCirGate() {}
 
   // Basic access method
   string getTypeStr() const { return _type; }
   size_t getId() const { return _id; }
+  size_t getTime() const { return _time; }
+  BitInfo getQubit(size_t qubit) const {
+    for (size_t i = 0; i < _qubits.size(); i++)
+    {
+      if(_qubits[i]._qubit==qubit) return _qubits[i];
+    }
+    cerr << "Not Found" << endl;
+    return _qubits[0];
+  }
+
+  void addQubit(size_t qubit, bool isTarget)
+  {
+    BitInfo temp = {._qubit = qubit, ._parent = NULL, ._child = NULL, ._isTarget = isTarget};
+    _qubits.push_back(temp);
+  }
+  void setTime(size_t time) { _time = time; }
+  void addParent(size_t qubit, QCirGate *p);
+  void addChild(size_t qubit, QCirGate *c);
   // Printing functions
   void printGate() const;
   void reportGate() const;
@@ -41,7 +67,9 @@ public:
 private:
   size_t _id;
   string _type;
-  vector<size_t> _pins;
+  size_t _nqubit;
+  size_t _time;
+  vector<BitInfo> _qubits;
 
 protected:
 };
