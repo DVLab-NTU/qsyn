ebec805f32c07cb84cc76ac4c3f16c72cbc50c0e
Joshua Lau <josh.lau2011@gmail.com>
2022-08-23 22:40:35 +0800

    :heavy_plus_sign: Adding branch for rational numbers

---
+++ src/util/rationalNum.h
@@ -0,0 +1,13 @@
+/****************************************************************************
+  FileName     [ myHashMap.h ]
+  PackageName  [ util ]
+  Synopsis     [ Define HashMap and Cache ADT ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2009-present LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+# ifndef RATIONAL_NUM_H
+# define RATIONAL_NUM_H
+
+// TODO: define rational number class
+
+# endif //RATIONAL_NUM_H
