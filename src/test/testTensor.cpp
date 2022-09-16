7edf67c7363759d4afeeaefb752d1de08b598100
JoshuaLau0220 <josh.lau2011@gmail.com>
2022-09-16 08:52:13 +0800

    :construction: Add cblas dependency for tensordot

--- src/test/testTensor.cpp
+++ src/test/testTensor.cpp
@@ -13,20 +13,31 @@
 #include <xtensor/xarray.hpp>
 #include <xtensor/xio.hpp>
 #include <xtensor-blas/xlinalg.hpp>
+#include <complex>
+#include <cmath>
 
 #include "catch2/catch.hpp"
 // #include "tensor.h"
 
-TEST_CASE("Tensors", "[Tensor]") {
-    xt::xarray<int> xt1 = xt::zeros<int>(std::vector<size_t>(3, 2));
-    
-    std::cout << xt1 << std::endl;
-}
-
 TEST_CASE("xtensor-blas", "[Tensor]") {
-    xt::xarray<double> a = {{3, 2, 1}, {0, 4, 2}, {1, 3, 5}};
-    auto d = xt::linalg::det(a);
-    std::cout << d << std::endl;  // 42.0
+    using namespace std::complex_literals;
+    // Z-spider
+    xt::xarray<std::complex<double>> a = xt::zeros<std::complex<double>>(std::vector<size_t>(3, 2));
+    a[std::vector<size_t>(3, 0)] = (1.0 + 0.0i);
+    a[std::vector<size_t>(3, 1)] = (1.0 + 0.0i);
+    // X-spider (*sqrt2)
+    xt::xarray<std::complex<double>> b = xt::zeros<std::complex<double>>(std::vector<size_t>(3, 2));
+    b(0, 0, 0) = 1.0;
+    b(0, 1, 1) = 1.0;
+    b(1, 0, 1) = 1.0;
+    b(1, 1, 0) = 1.0;
+    // ZX connection
+    auto c = xt::linalg::tensordot(a, b, {0}, {0});
+    std::cout << c << std::endl;
+    // matrixfy
+    xt::xarray<std::complex<double>> d = xt::transpose(c, {0, 2, 1, 3});
+    auto e = d.reshape({4, 4});
+    std::cout << e << std::endl;
 }
 // nested_initializer_list_t<int, 2> t = {{1, 2, 3}, {4, 5, 6}};
 // Tensor<int> t1(t);
