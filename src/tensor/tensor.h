a655252e4e12064a57edfbf4d663d8b1e28ccf9f
JoshuaLau0220 <josh.lau2011@gmail.com>
2022-09-19 16:00:27 +0800

    :sparkles: H-boxes; scalar adjustment

--- src/tensor/tensor.h
+++ src/tensor/tensor.h
@@ -32,8 +32,6 @@ template <typename T>
 class Tensor {
     using DataType = std::complex<T>;
     using InternalType = xt::xarray<DataType>;
-    
-
 public:
     Tensor(const TensorShape& shape) : _tensor(shape) {}
     Tensor(TensorShape&& shape) : _tensor(shape) {}
@@ -43,77 +41,54 @@ public:
     template <typename From>
     requires std::convertible_to<From, InternalType>
     Tensor(From&& internal) : _tensor(internal) {}
-
-    friend std::ostream& operator<<(std::ostream& os, const Tensor<T>& t) {
-        return os << t._tensor;
-    }
+    
+    
+    template <typename U>
+    friend std::ostream& operator<<(std::ostream& os, const Tensor<U>& t); // different typename to avoid shadowing
 
     template <typename... Args>
-    DataType& operator()(const Args&... args) {
-        return _tensor(args...);
-    }
+    DataType& operator()(const Args&... args);
     template <typename... Args>
-    const DataType& operator()(const Args&... args) const {
-        return _tensor(args...);
-    }
+    const DataType& operator()(const Args&... args) const;
 
-    DataType& operator[](const TensorIndex& i) {
-        return _tensor[i];
-    }
-    const DataType& operator[](const TensorIndex& i) const {
-        return _tensor[i];
-    }
+    DataType& operator[](const TensorIndex& i);
+    const DataType& operator[](const TensorIndex& i) const;
 
-    bool operator== (const Tensor<T>& rhs) const {
-        return _tensor == rhs._tensor;
-    }
+    bool operator== (const Tensor<T>& rhs) const;
+    bool operator!= (const Tensor<T>& rhs) const;
 
-    bool operator!= (const Tensor<T>& rhs) const {
-        return _tensor != rhs._tensor;
-    }
+    Tensor<T>& operator+=(const Tensor<T>& rhs);
+    Tensor<T>& operator-=(const Tensor<T>& rhs);
+    Tensor<T>& operator*=(const Tensor<T>& rhs);
+    Tensor<T>& operator/=(const Tensor<T>& rhs);
+
+    template <typename U>
+    friend Tensor<U> operator+(Tensor<U> lhs, const Tensor<U>& rhs);
+    template <typename U>
+    friend Tensor<U> operator-(Tensor<U> lhs, const Tensor<U>& rhs);
+    template <typename U>
+    friend Tensor<U> operator*(Tensor<U> lhs, const Tensor<U>& rhs);
+    template <typename U>
+    friend Tensor<U> operator/(Tensor<U> lhs, const Tensor<U>& rhs);
 
     // Generate an tensor that corresponds to a n-qubit identity gate.
     // Note that `n` is the number of qubits, not dimensions
-    static Tensor<T> identity(const size_t& n) {
-        InternalType t = xt::eye<DataType>(intPow(2, n));
-        t.reshape(TensorShape(2*n, 2));
-        return t;
-    }
+    static Tensor<T> identity(const size_t& n);
 
     // Generate an tensor that corresponds to a n-qubit Z-spider.
-    static Tensor<T> zspider(const size_t& n, const Phase& phase) {
-        Tensor<T> t = xt::zeros<DataType>(TensorShape(n, 2));
-        if (n == 0) {
-            t() = 1. + std::exp(1.0i * phase.toFloatType<T>());
-        } else {
-            t[TensorIndex(n, 0)] = 1.;
-            t[TensorIndex(n, 1)] = std::exp(1.0i * phase.toFloatType<T>());
-        }
-        return t;
-    }
+    static Tensor<T> zspider(const size_t& n, const Phase& phase = Phase(0));
 
     // Generate an tensor that corresponds to a n-qubit X-spider.
-    static Tensor<T> xspider(const size_t& n, const Phase& phase) {
-        Tensor<T> t0 = xt::ones<DataType>(TensorShape(n, 2));
-        Tensor<T> ketMinus(TensorShape{2});
-        ketMinus(0, 0) = 1.;
-        ketMinus(0, 1) = -1.;
-        Tensor<T> t1 = tensorPow(ketMinus, n);
-        t0._tensor += t1._tensor * std::exp(1.0i * phase.toFloatType<T>());
-        t0._tensor /= std::pow(std::sqrt(2), n);
-        return t0;
-    }
+    static Tensor<T> xspider(const size_t& n, const Phase& phase = Phase(0));
+
+    // Generate an tensor that corresponds to a n-qubit H-box.
+    // The t(1, ..., 1) element is set to be `exp(i*phase)`
+    static Tensor<T> hbox(const size_t& n, const DataType& a = -1.);
 
     // tensor-dot two tensors
     // dots the two tensors along the axes in ax1 and ax2
     static Tensor<T> tensordot(const Tensor<T>& t1, const Tensor<T>& t2,
-                               const TensorAxisList& ax1 = {}, const TensorAxisList& ax2 = {}) {
-        if (ax1.size() != ax2.size()) {
-            throw std::invalid_argument("The two index orders should contain the same number of indices.");
-        }
-        Tensor<T> ret = xt::linalg::tensordot(t1._tensor, t2._tensor, ax1, ax2);
-        return ret;
-    }
+                               const TensorAxisList& ax1 = {}, const TensorAxisList& ax2 = {});
     // tensor-dot a tensor between pairs of axes
     // dots the tensor  along the axes in ax1 and ax2
     static Tensor<T> selfTensordot(const Tensor<T>& t, const TensorAxisList& ax1 = {}, const TensorAxisList& ax2 = {});
@@ -137,13 +112,151 @@ private:
     static TensorAxisList concatAxisList(const TensorAxisList& ax1, const TensorAxisList& ax2);
     // Calculate tensor powers
     static Tensor<T> tensorPow(const Tensor<T>& t, size_t n);
+
+    static DataType nuPow(const int& n);
 };
 
+//------------------------------
+// Operators
+//------------------------------
 
+template <typename U>
+std::ostream& operator<<(std::ostream& os, const Tensor<U>& t) {
+    return os << t._tensor;
+}
+
+template <typename T>
+template <typename... Args>
+Tensor<T>::DataType& Tensor<T>::operator()(const Args&... args) {
+    return _tensor(args...);
+}
+template <typename T>
+template <typename... Args>
+const Tensor<T>::DataType& Tensor<T>::operator()(const Args&... args) const {
+    return _tensor(args...);
+}
+
+
+template<typename T>
+Tensor<T>::DataType& Tensor<T>::operator[](const TensorIndex& i) {
+    return _tensor[i];
+}
+template<typename T>
+const Tensor<T>::DataType& Tensor<T>::operator[](const TensorIndex& i) const {
+    return _tensor[i];
+}
+template<typename T>
+bool Tensor<T>::operator== (const Tensor<T>& rhs) const {
+    return _tensor == rhs._tensor;
+}
+template<typename T>
+bool Tensor<T>::operator!= (const Tensor<T>& rhs) const {
+    return _tensor != rhs._tensor;
+}
+
+template<typename T>
+Tensor<T>& Tensor<T>::operator+=(const Tensor<T>& rhs) {
+    _tensor += rhs._tensor;
+    return *this;
+}
+
+template<typename T>
+Tensor<T>& Tensor<T>::operator-=(const Tensor<T>& rhs) {
+    _tensor -= rhs._tensor;
+    return *this;
+}
+
+template<typename T>
+Tensor<T>& Tensor<T>::operator*=(const Tensor<T>& rhs) {
+    _tensor *= rhs._tensor;
+    return *this;
+}
+
+template<typename T>
+Tensor<T>& Tensor<T>::operator/=(const Tensor<T>& rhs) {
+    _tensor /= rhs._tensor;
+    return *this;
+}
+
+template<typename U>
+Tensor<U> operator+(Tensor<U> lhs, const Tensor<U>& rhs) {
+    lhs._tensor += rhs._tensor;
+    return lhs;
+}
+template<typename U>
+Tensor<U> operator-(Tensor<U> lhs, const Tensor<U>& rhs) {
+    lhs._tensor -= rhs._tensor;
+    return lhs;
+}
+template<typename U>
+Tensor<U> operator*(Tensor<U> lhs, const Tensor<U>& rhs) {
+    lhs._tensor *= rhs._tensor;
+    return lhs;
+}
+template<typename U>
+Tensor<U> operator/(Tensor<U> lhs, const Tensor<U>& rhs) {
+    lhs._tensor /= rhs._tensor;
+    return lhs;
+}
 
 //------------------------------
 // Tensor manipulation functions
 //------------------------------
+template<typename T>
+Tensor<T> Tensor<T>::identity(const size_t& n) {
+    InternalType t = xt::eye<DataType>(intPow(2, n));
+    t.reshape(TensorShape(2*n, 2));
+    return t;
+}
+
+template<typename T>
+Tensor<T> Tensor<T>::zspider(const size_t& n, const Phase& phase) {
+    Tensor<T> t = xt::zeros<Tensor<T>::DataType>(TensorShape(n, 2));
+    if (n == 0) {
+        t() = 1. + std::exp(1.0i * phase.toFloatType<T>());
+    } else {
+        t[TensorIndex(n, 0)] = 1.;
+        t[TensorIndex(n, 1)] = std::exp(1.0i * phase.toFloatType<T>());
+    }
+    t._tensor *= nuPow(2 - n);
+    return t;
+}
+
+template<typename T>
+Tensor<T> Tensor<T>::xspider(const size_t& n, const Phase& phase) {
+    Tensor<T> t = xt::ones<Tensor<T>::DataType>(TensorShape(n, 2));
+    Tensor<T> ketMinus(TensorShape{2});
+    ketMinus(0, 0) = 1.;
+    ketMinus(0, 1) = -1.;
+    Tensor<T> tmp = tensorPow(ketMinus, n);
+    t._tensor += tmp._tensor * std::exp(1.0i * phase.toFloatType<T>());
+    t._tensor /= std::pow(std::sqrt(2), n);
+    t._tensor *= nuPow(2 - n);
+    return t;
+}
+
+template<typename T>
+Tensor<T> Tensor<T>::hbox(const size_t& n, const Tensor<T>::DataType& a) {
+    Tensor<T> t = xt::ones<Tensor<T>::DataType>(TensorShape(n, 2));
+    if (n == 0) {
+        t() = a;
+    } else {
+        t[TensorIndex(n, 1)] = a;
+    }
+    t._tensor *= nuPow(n);
+    return t; 
+}
+
+
+template<typename T>
+Tensor<T> Tensor<T>::tensordot(const Tensor<T>& t1, const Tensor<T>& t2,
+                            const TensorAxisList& ax1, const TensorAxisList& ax2) {
+    if (ax1.size() != ax2.size()) {
+        throw std::invalid_argument("The two index orders should contain the same number of indices.");
+    }
+    Tensor<T> t = xt::linalg::tensordot(t1._tensor, t2._tensor, ax1, ax2);
+    return t;
+}
 template<typename T>
 Tensor<T> Tensor<T>::selfTensordot(const Tensor<T>& t, const TensorAxisList& ax1, const TensorAxisList& ax2) {
     if (ax1.size() != ax2.size()) {
@@ -156,7 +269,9 @@ Tensor<T> Tensor<T>::selfTensordot(const Tensor<T>& t, const TensorAxisList& ax1
     auto tmp = Tensor<T>::identity(ax1.size());
     auto tmpOrder = TensorAxisList(ax1.size() + ax2.size(), 0);
     std::iota(tmpOrder.begin(), tmpOrder.end(), 0);
-    return tensordot(t, tmp, concatAxisList(ax1, ax2), tmpOrder);
+    Tensor<T> u = tensordot(t, tmp, concatAxisList(ax1, ax2), tmpOrder);
+    u._tensor *= nuPow(2 * ax1.size());
+    return u;
 }
 
 
@@ -165,9 +280,9 @@ Tensor<T> Tensor<T>::toMatrix(const TensorAxisList& axin, const TensorAxisList& 
     if (!isPartition(*this, axin, axout)) {
         throw std::invalid_argument("The two axis lists should partition 0~(n-1).");
     }
-    Tensor<T> ret = xt::transpose(_tensor, concatAxisList(axin, axout));
-    ret._tensor.reshape({intPow(2, axin.size()), intPow(2, axout.size())});
-    return ret;
+    Tensor<T> t = xt::transpose(_tensor, concatAxisList(axin, axout));
+    t._tensor.reshape({intPow(2, axin.size()), intPow(2, axout.size())});
+    return t;
 }
 //------------------------------
 // Private member functions
@@ -186,9 +301,9 @@ bool Tensor<T>::isPartition(const Tensor<T>& t, const TensorAxisList& axin, cons
 
 template<typename T>
 TensorAxisList Tensor<T>::concatAxisList(const TensorAxisList& ax1, const TensorAxisList& ax2) {
-    TensorAxisList ret = ax1;
-    ret.insert(ret.end(), ax2.begin(), ax2.end());
-    return ret;
+    TensorAxisList ax = ax1;
+    ax.insert(ax.end(), ax2.begin(), ax2.end());
+    return ax;
 }
 
 template<typename T>
@@ -200,6 +315,11 @@ Tensor<T> Tensor<T>::tensorPow(const Tensor<T>& t, size_t n) {
         return Tensor<T>::tensordot(tmp, tmp);
     else 
         return Tensor<T>::tensordot(t, Tensor<T>::tensordot(tmp, tmp));
+}
+
+template<typename T>
+Tensor<T>::DataType Tensor<T>::nuPow(const int& n) {
+    return std::pow(2., -0.25 * n);
 }
 
 #endif  // TENSOR_H
