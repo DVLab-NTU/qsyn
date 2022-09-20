91e22827a95b9d2a55392587153b91e9ecf50cb3
anitalu724 <r10921058@ntu.edu.tw>
2022-09-20 21:29:33 +0800

    :sparkles: Update #104 Simplify Definition / Rules Definition

--- src/graph/zxRules.h
+++ src/graph/zxRules.h
@@ -10,19 +10,35 @@
 #ifndef ZX_RULES_H
 #define ZX_RULES_H
 
+#include <vector>
+#include <iostream>
+#include "zxDef.h"
+#include "zxGraph.h"
+
 class zxRules{
+  typedef pair<int, int> MatchType;
+  typedef vector<MatchType> MatchTypeVec;
+
     public:
         zxRules(){}
         ~zxRules(){}
+
     private:
 
 };
 
 class spiderFusion : public zxRules{
+  typedef pair<ZXVertex*, ZXVertex*> MatchType;
+  typedef vector<MatchType> MatchTypeVec;
 
+  public:
+    MatchTypeVec match(ZXGraph* g);
+    
+  private:
 };
 
 class bialgebra : public zxRules{
+
 
 };
 
