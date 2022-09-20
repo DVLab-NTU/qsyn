19ec5d41927e2c7a2985c8b9d4b1dc9638ce0e8d
anitalu724 <r10921058@ntu.edu.tw>
2022-09-20 18:59:48 +0800

    :sparkles: Update #104 add polymorphism

---
+++ src/graph/zxRules.h
@@ -0,0 +1,30 @@
+/****************************************************************************
+  FileName     [ zxRules.h ]
+  PackageName  [ graph ]
+  Synopsis     [ ZX Basic Rules ]
+  Author       [ Cheng-Hua Lu ]
+  Copyright    [ Copyleft(c) 2022-present DVLab, GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+
+#ifndef ZX_RULES_H
+#define ZX_RULES_H
+
+class zxRules{
+    public:
+        zxRules(){}
+        ~zxRules(){}
+    private:
+
+};
+
+class spiderFusion : public zxRules{
+
+};
+
+class bialgebra : public zxRules{
+
+};
+
+
+#endif
