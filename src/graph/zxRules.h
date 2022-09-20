c97867d0d66e2807a2e5ee4a34f5d40f667623fe
anitalu724 <r10921058@ntu.edu.tw>
2022-09-20 18:31:57 +0800

    :sparkles: Update #104

---
+++ src/graph/zxRules.h
@@ -0,0 +1,15 @@
+/****************************************************************************
+  FileName     [ zxRules.h ]
+  PackageName  [ graph ]
+  Synopsis     [ ZX Basic Rules ]
+  Author       [ Cheng-Hua Lu ]
+  Copyright    [ Copyleft(c) 2022-present DVLab, GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+
+#ifndef ZX_RULES_H
+#define ZX_RULES_H
+
+
+
+#endif
