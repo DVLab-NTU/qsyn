0bfe7ff89cf850f2d590731c247e10e83d11fb87
chinyi0523 <james31423a@gmail.com>
2022-10-24 21:44:43 +0800

    :sparkles: Hopf (Finish #123 #170)

--- src/simplifier/hopf.cpp
+++ src/simplifier/hopf.cpp
@@ -28,6 +28,42 @@ void Hopf::match(ZXGraph* g){
   for(size_t i = 0; i < Vertices.size(); i++) Vertex2idx[Vertices[i]] = i;
   vector<bool> validVertex(Vertices.size(), true);
   
+  for(size_t i=0; i< g->getNumVertices(); i++){
+    ZXVertex* v = Vertices[i];
+    if(v->getType()==VertexType::BOUNDARY){
+      validVertex[Vertex2idx[v]] = false;
+      continue;
+    }
+    if(!validVertex[Vertex2idx[v]]) continue;
+    vector<ZXVertex*> neighbors = v->getNeighbors();
+    NeighborMap nbm = Vertices[i]->getNeighborMap();
+    
+    for(size_t j=0; j<neighbors.size(); j++){
+      if(neighbors[j]->getType()==VertexType::BOUNDARY) continue;
+      bool sameColor = v->getType() == neighbors[j]->getType();
+      size_t targetEdges=0;
+      for(auto itr = nbm.begin(); itr != nbm.end(); itr++){
+        if(itr->first == neighbors[j]){
+          if(targetEdges >=2) break;
+          if(sameColor){
+            if(*(itr->second) == EdgeType::HADAMARD)
+              targetEdges++;
+          }
+          else{
+            if(*(itr->second) == EdgeType::SIMPLE)
+              targetEdges++;
+          }
+        }
+      }
+      if(targetEdges >=2){
+        _matchTypeVec.push_back(make_pair(v, neighbors[j]));
+        validVertex[Vertex2idx[v]] = false;
+        validVertex[Vertex2idx[neighbors[j]]] = false;
+        break;
+      } 
+    }
+  }
+
   setMatchTypeVecNum(_matchTypeVec.size());
 }
 
@@ -47,5 +83,40 @@ void Hopf::rewrite(ZXGraph* g){
   //* _edgeTableValues: A pair of int like (int s, int h), which means #s EdgeType::SIMPLE and #h EdgeType::HADAMARD
   
   // Need to update global scalar and phase
-    
+  for(size_t i=0; i<_matchTypeVec.size(); i++){
+    bool sameColor = _matchTypeVec[i].first->getType() == _matchTypeVec[i].second->getType();
+    if(sameColor){
+      NeighborMap nbm = _matchTypeVec[i].first->getNeighborMap();
+      auto results = nbm.equal_range(_matchTypeVec[i].second);
+      EdgeType* tmp = nullptr;
+      size_t n=0;
+      for(auto itr=results.first; itr!=results.second; itr++){
+        if(*(itr->second)==EdgeType::HADAMARD){
+          n++;
+          if(n%2 == 0){
+            _removeEdges.push_back(make_pair(make_pair(_matchTypeVec[i].first, _matchTypeVec[i].second), tmp));
+            _removeEdges.push_back(make_pair(make_pair(_matchTypeVec[i].first, _matchTypeVec[i].second), itr->second));
+          }
+          tmp = itr->second;
+        }
+      }
+    }
+    else{
+      NeighborMap nbm = _matchTypeVec[i].first->getNeighborMap();
+      auto results = nbm.equal_range(_matchTypeVec[i].second);
+      EdgeType* tmp = nullptr;
+      size_t n=0;
+      for(auto itr=results.first; itr!=results.second; itr++){
+        if(*(itr->second)==EdgeType::SIMPLE){
+          n++;
+          if(n%2 == 0){
+            _removeEdges.push_back(make_pair(make_pair(_matchTypeVec[i].first, _matchTypeVec[i].second), tmp));
+            _removeEdges.push_back(make_pair(make_pair(_matchTypeVec[i].first, _matchTypeVec[i].second), itr->second));
+          }
+          tmp = itr->second;
+        }
+      }
+    }
+  }  
+  cout << _removeEdges.size() << endl;
 }
