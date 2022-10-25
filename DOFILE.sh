4f11bc2d1453dd95e98218510e5cedc83dcaa1d4
JoshuaLau0220 <josh.lau2011@gmail.com>
2022-10-26 04:24:22 +0800

    🕹  Control options for dofile scripts

--- DOFILE.sh
+++ DOFILE.sh
@@ -1,56 +1,115 @@
 #!/bin/bash
-# TODO add verbose-diff mode
-# TODO add no-color mode
-USAGE_STR="Usage:\t./RUN_DOFILE.sh <path/to/dofile> <-Diff|-UPdate>\n\t-d : Compare the result to the reference\n\t-up: Update the reference file\n"
+source qsyn_testing_code/BASH_UTIL.sh
+
 RETURN_CODE=0
-if [ $# -ne 2 ]; then 
-    echo -e ${USAGE_STR}
-    exit 1; 
+
+function USAGE () {
+    printf "%s\t%-12s%-17s%-16s%-9s%s\n" "Usage:" "./DOFILE.sh" "<path/to/dofile>" "[-Diff|-UPdate]" "[-Quiet]" "[-NOColor]" 
+    printf "\t%-9s%-2s%-12s\n" "-Diff" ":" "Compare the result to the reference"
+    printf "\t%-9s%-2s%-12s\n" "-UPdate" ":" "Update the reference file"
+    printf "\t%-9s%-2s%-12s\n" "" "" "If neither -Diff or -UPdate is specified, default to -Diff mode."
+    printf "\t%-9s%-2s%-12s\n" "-Quiet " ":" "Don't print diff message"
+    printf "\t%-9s%-2s%-12s\n" "-NOColor" ":" "Turn off colored output"
+    printf "\t%-9s%-2s%-12s\n" "-Help" ":" "Print this help message"
+    exit 1
+} >&2
+
+
+if (( $# < 1 || $# > 4 )); then 
+    USAGE
 fi
 
-FILE_PATH=$1
-if [ ! -f $FILE_PATH ]; then 
-    echo -e "Error\tInvalid dofile path!"
-    exit -1
-fi
+DODIFF=0
+DOUPDATE=0
+DOQUIET=0
+DOCOLOR=1
+DOHELP=0
+
+for TOKEN in "$@"; do
+    if [[ $(MATCH_STR "$TOKEN" "-help" 2) == 1 ]]; then 
+        USAGE
+    fi
+done
+
+for TOKEN in "$@"; do
+    if [[ $(MATCH_STR "$TOKEN" "-diff" 2) == 1 ]]; then
+        if [[ $DOUPDATE == 1 ]]; then
+            printf "Error: Cannot use -Diff and -UPdate at the same time!!\n"
+            exit 1
+        fi 
+        if [[ $DODIFF == 1 ]]; then
+            printf "Error: extra option (%s)!!\n" $TOKEN
+            exit 1
+        fi 
+        DODIFF=1
+    elif [[ $(MATCH_STR "$TOKEN" "-update" 3) == 1 ]]; then
+        if [[ $DODIFF == 1 ]]; then
+            echo "Error: Cannot use -Diff and -UPdate at the same time!!"
+            exit 1
+        fi 
+        if [[ $DOUPDATE == 1 ]]; then
+            printf "Error: Extra Option (%s)!!\n" $TOKEN
+            exit 1
+        fi 
+        DOUPDATE=1
+    elif [[ $(MATCH_STR "$TOKEN" "-quiet" 2) == 1 ]]; then
+        if [[ $DOQUIET == 1 ]]; then
+            printf "Error: Extra Option (%s)!!\n" $TOKEN
+            exit 1
+        fi 
+        DOQUIET=1
+    elif [[ $(MATCH_STR "$TOKEN" "-nocolor" 4) == 1 ]]; then
+        if [[ $DOCOLOR == 0 ]]; then
+            printf "Error: Extra Option (%s)!!\n" $TOKEN
+            exit 1
+        fi 
+        DOCOLOR=0
+    elif [ -f $TOKEN ]; then
+        FILE_PATH=$TOKEN
+    else 
+        printf "Error: invalid option!! (%s)\n" $TOKEN
+        exit 1
+    fi
+done
+
+if [[ $DODIFF == 0 && $DOUPDATE == 0 ]]; then DODIFF=1; fi
 
 DOFILE=$(basename $FILE_PATH)
 DOFILE_DIR=$(dirname $FILE_PATH)
 PKG_NAME=$(dirname $DOFILE_DIR)
 REF_FILE=${PKG_NAME}/ref/${DOFILE%%.*}.log
+PASS_TEXT="Passed"
+FAIL_TEXT="Failed"
+DIFF_HEAD="------------------- Differences --------------------"
+DIFF_FOOT="----------------------------------------------------"
 
-MODE_ARG=$(echo $2 | awk '{print tolower($0)}')
-DIFF_STR="-diff"
-DIFF_STR_AT_LEAST="-d"
-UPDATE_STR="-update"
-UPDATE_STR_AT_LEAST="-up"
+if [[ $DOCOLOR == 1 ]]; then
+    PASS_TEXT="${BOLD}${GREEN}${PASS_TEXT}${WHITE}${NORMAL}"
+    FAIL_TEXT="${BOLD}${RED}${FAIL_TEXT}${WHITE}${NORMAL}"
+    DIFF_HEAD="${BOLD}${RED}${DIFF_HEAD}${WHITE}${NORMAL}"
+    DIFF_FOOT="${BOLD}${RED}${DIFF_FOOT}${WHITE}${NORMAL}"
+fi
 
-BOLD=$(tput bold)
-NORMAL=$(tput sgr0)
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-WHITE=$(tput sgr0)
-PASS_TEXT="${BOLD}${GREEN}Passed${WHITE}${NORMAL}"
-FAIL_TEXT="${BOLD}${RED}Failed${WHITE}${NORMAL}"
-
-if [[ "$DIFF_STR" == *"$MODE_ARG"* ]] && \
-   [[ "$MODE_ARG" == *"$DIFF_STR_AT_LEAST"* ]]; then
+if [[ $DODIFF == 1 ]]; then
     printf "Testing %-30s" ${DOFILE}
-    ./qsyn -f ${FILE_PATH} |& diff ${REF_FILE} - > /dev/null 2>&1 
+    OUTPUT=$(./qsyn -f ${FILE_PATH} |& diff ${REF_FILE} - )
     STATUS=$?
-    if [ $STATUS -eq 0 ]; then 
-        echo ${PASS_TEXT};
+    if [[ $STATUS == 0 ]]; then 
+        echo -e ${PASS_TEXT};
     else 
-        echo -e ${FAIL_TEXT}; RETURN_CODE=1;
+        echo -e ${FAIL_TEXT}
+         RETURN_CODE=1
+        if [[ $DOQUIET == 0 ]]; then
+            echo -e "${DIFF_HEAD}"
+            echo -e "${OUTPUT}"
+            echo -e "${DIFF_FOOT}"
+        fi
     fi
-elif [[ "$UPDATE_STR" == *"$MODE_ARG"* ]] && \
-        [[ "$MODE_ARG" == *"$UPDATE_STR_AT_LEAST"* ]]; then
+elif [[ $DOUPDATE == 1 ]]; then
     printf "Updating %s\n" $(basename ${REF_FILE})
     ./qsyn -f ${FILE_PATH} > ${REF_FILE} 2>&1
 else 
-    echo -e "Error:\tInvalid options!"
-    echo -e ${USAGE_STR}
-    exit -1; 
+    echo -e "Error"
+    USAGE
 fi
-
 exit ${RETURN_CODE}
