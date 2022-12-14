bf100f4855e1f84edc18ea585bc5f91f401eb842
JoshuaLau0220 <josh.lau2011@gmail.com>
2022-12-14 22:50:43 +0800

    :recycle: clean up m2.cpp (constness, pass by ref, ...)

--- src/m2/m2.cpp
+++ src/m2/m2.cpp
@@ -11,186 +11,207 @@
 extern size_t verbose;
 using namespace std;
 
-Row operator+(Row& lhs, const Row& rhs){
-  lhs += rhs;
-  return lhs;
+Row operator+(Row& lhs, const Row& rhs) {
+    lhs += rhs;
+    return lhs;
 }
 
-Row &Row::operator+=(const Row& rhs) {
-    assert( _row.size() == rhs._row.size());
-    for(size_t i=0; i<_row.size(); i++){
-      _row[i] = (_row[i] + rhs._row[i])%2; 
+Row& Row::operator+=(const Row& rhs) {
+    assert(_row.size() == rhs._row.size());
+    for (size_t i = 0; i < _row.size(); i++) {
+        _row[i] = (_row[i] + rhs._row[i]) % 2;
     }
     return *this;
 }
 
-/// @brief Print row
+/**
+ * @brief Print row
+ *
+ */
 void Row::printRow() const {
-  for(auto e: _row){
-    cout << unsigned(e) << " ";
-  }
-  cout << endl;
-}
-
-/// @brief Check is one hot
-/// @return 
-bool Row::isOneHot() {
-  size_t cnt = 0;
-  for(auto i: _row){
-    if(i==1){
-      if(cnt==1) return false;
-      cnt += 1;
+    for (auto e : _row) {
+        cout << unsigned(e) << " ";
     }
-  }
-  if(cnt!=1) return false;
-  else return true;
+    cout << endl;
 }
 
-/// @brief Print matrix
+/**
+ * @brief @brief Check is one hot
+ *
+ * @return true if one hot
+ * @return false if not
+ */
+bool Row::isOneHot() const {
+    size_t cnt = 0;
+    for (auto& i : _row) {
+        if (i == 1) {
+            if (cnt == 1) return false;
+            ++cnt;
+        }
+    }
+    return (cnt == 1);
+}
+
+/**
+ * @brief @brief Print matrix
+ *
+ */
 void M2::printMatrix() const {
-  cout << "M2 matrix:" << endl;
-  for(const auto row: _matrix){
-    row.printRow();
-  }
-  cout << endl;
+    cout << "M2 matrix:" << endl;
+    for (const auto& row : _matrix) {
+        row.printRow();
+    }
+    cout << endl;
 }
 
-/// @brief Print the operations
+/**
+ * @brief Print the operation track
+ *
+ */
 void M2::printTrack() const {
-  cout << "Track:" << endl;
-  for(size_t i=0; i < _opStorage.size(); i++){
-    cout << "Step " << i+1 << ": " << _opStorage[i].first << " to " << _opStorage[i].second << endl;
-  }
-  cout << endl;
+    cout << "Track:" << endl;
+    for (size_t i = 0; i < _opStorage.size(); i++) {
+        cout << "Step " << i + 1 << ": " << _opStorage[i].first << " to " << _opStorage[i].second << endl;
+    }
+    cout << endl;
 }
 
-void M2::defaultInit(){
-  _matrix.push_back(Row(0, vector<unsigned char>{1,0,1,1,0,1}));
-  _matrix.push_back(Row(1, vector<unsigned char>{0,1,1,1,0,0}));
-  _matrix.push_back(Row(2, vector<unsigned char>{0,1,1,0,1,0}));
-  _matrix.push_back(Row(3, vector<unsigned char>{1,0,0,1,1,0}));
-  _matrix.push_back(Row(4, vector<unsigned char>{1,1,0,1,1,0}));
-  _matrix.push_back(Row(5, vector<unsigned char>{0,0,0,1,0,1}));
+/**
+ * @brief initialize a test matrix
+ *
+ */
+void M2::defaultInit() {
+    _matrix.push_back(Row(0, vector<unsigned char>{1, 0, 1, 1, 0, 1}));
+    _matrix.push_back(Row(1, vector<unsigned char>{0, 1, 1, 1, 0, 0}));
+    _matrix.push_back(Row(2, vector<unsigned char>{0, 1, 1, 0, 1, 0}));
+    _matrix.push_back(Row(3, vector<unsigned char>{1, 0, 0, 1, 1, 0}));
+    _matrix.push_back(Row(4, vector<unsigned char>{1, 1, 0, 1, 1, 0}));
+    _matrix.push_back(Row(5, vector<unsigned char>{0, 0, 0, 1, 0, 1}));
 }
 
-/// @brief Perform XOR operation
-/// @param ctrl 
-/// @param targ (the target)
-/// @param track 
-/// @return true if successfully XORed, false if not
-bool M2::xorOper(size_t ctrl, size_t targ, bool track)  { 
-  if( ctrl >= _matrix.size() ) {
-    cerr << "Error: wrong dimension " << ctrl << endl;
-    return false;
-  }
-  if( targ >= _matrix.size() ) {
-    cerr << "Error: wrong dimension " << targ << endl;
-    return false;
-  }
-  _matrix[targ] += _matrix[ctrl]; 
-  if(track) _opStorage.emplace_back(ctrl, targ);
-  return true;
+/**
+ * @brief Perform XOR operation
+ * @param ctrl the control
+ * @param targ the target
+ * @param track if true, record the process to operation track
+ *
+ * @return true if successfully XORed,
+ * @return false if not
+ */
+bool M2::xorOper(size_t ctrl, size_t targ, bool track) {
+    if (ctrl >= _matrix.size()) {
+        cerr << "Error: wrong dimension " << ctrl << endl;
+        return false;
+    }
+    if (targ >= _matrix.size()) {
+        cerr << "Error: wrong dimension " << targ << endl;
+        return false;
+    }
+    _matrix[targ] += _matrix[ctrl];
+    if (track) _opStorage.emplace_back(ctrl, targ);
+    return true;
 }
 
-/// @brief Perform Gaussian Elimination
-void M2::gaussianElim(bool track){
-    if(verbose>=3) cout << "Performing Gaussian Elimination..." << endl;
-    if(verbose>=8) printMatrix();
+/**
+ * @brief Perform Gaussian Elimination
+ *
+ * @param track if true, record the process to operation track
+ */
+void M2::gaussianElim(bool track) {
+    if (verbose >= 3) cout << "Performing Gaussian Elimination..." << endl;
+    if (verbose >= 8) printMatrix();
     _opStorage.clear();
-    for(size_t i=0; i < _matrix.size() - 1; i++){
-      if(_matrix[i].getRow()[i] == 0){
-        bool noSol = true; 
-        for(size_t j=i+1; j<_matrix.size(); j++){
-          if(_matrix[j].getRow()[i] ==1){
-            xorOper(j, i, track);
-            if(verbose>=8) {
-              cout << "Diag Add " << j << " to "<< i << endl;
-              printMatrix();
+    for (size_t i = 0; i < _matrix.size() - 1; i++) {
+        // If a number on main diagonal is zero, greedily perform row operations to make them 1
+        if (_matrix[i][i] == 0) {
+            bool noSol = true;
+            for (size_t j = i + 1; j < _matrix.size(); j++) {
+                if (_matrix[j][i] == 1) {
+                    xorOper(j, i, track);
+                    if (verbose >= 8) {
+                        cout << "Diag Add " << j << " to " << i << endl;
+                        printMatrix();
+                    }
+                    noSol = false;
+                    break;
+                }
             }
-            noSol = false;
-          }
-          break;
+            if (noSol) {
+                cout << "No Solution" << endl;
+                return;
+            }
         }
-        if(noSol) { cout << "No Solution" << endl; return;}
-		  }
-		  for(size_t j=i+1; j < _matrix.size(); j++){
-        if(_matrix[j].getRow()[i] ==1 && _matrix[i].getRow()[i] ==1){
-          xorOper(i, j, track);
-          if(verbose>=8) {
-            cout << "Add " << i << " to "<< j << endl;
-            printMatrix();
-          }
+
+        // convert to upper-triangle matrix
+        for (size_t j = i + 1; j < _matrix.size(); j++) {
+            if (_matrix[j][i] == 1 && _matrix[i][i] == 1) {
+                xorOper(i, j, track);
+                if (verbose >= 8) {
+                    cout << "Add " << i << " to " << j << endl;
+                    printMatrix();
+                }
+            }
         }
-		  }
-	  }
-    for(size_t i = 0; i < _matrix.size(); i++){
-		  for(size_t j=_matrix.size()-i; j < _matrix.size(); j++){
-        if(_matrix[_matrix.size()-i-1].getRow()[j] ==1){
-          xorOper(j, _matrix.size()-i-1, track);
-          if(verbose>=8) printMatrix();
+    }
+
+    // convert to identity matrix on the leftmost _matrix.size() matrix
+    for (size_t i = 0; i < _matrix.size(); i++) {
+        for (size_t j = _matrix.size() - i; j < _matrix.size(); j++) {
+            if (_matrix[_matrix.size() - i - 1][j] == 1) {
+                xorOper(j, _matrix.size() - i - 1, track);
+                if (verbose >= 8) {
+                    cout << "Add " << j << " to " << _matrix.size() - i - 1 << endl;
+                    printMatrix();
+                }
+            }
         }
-		  }
-	  }
+    }
 }
 
 /// @brief Check m2 is identity
-/// @return 
-bool M2::isIdentity(){
-  //REVIEW - Only check one 1 in a row
-  for(auto row: _matrix){
-    if(!row.isOneHot()) return false;
-  }
-  return true;
-}
-
-// /// @brief Build matrix from bitsets
-// /// @param bitsets 
-// /// @return true
-// bool M2::fromBitsets(const vector<bitset<16>>& bitsets){
-//   for(auto bitset: bitsets){
-//     _matrix.push_back(Row(_matrix.size(), _size, bitset));
-//   }
-//   return true;
-// }
-
-/// @brief Build matrix from ZX-graph (according to the given order)
-/// @param frontier 
-/// @param neighbors 
-/// @return true if successfully built, false if not
-bool M2::fromZXVertices(const ZXVertexList& frontier, const ZXVertexList& neighbors){
-  if(frontier.size() != neighbors.size()){
-    cout << "Numbers of elements in frontier and neighbors mismatch!" << endl;
-    return false;
-  }
-  //NOTE - assign row by calculating a Frontier's connecting status to Neighbors, e.g. 10010 = connect to qubit 0 and 3.
-  
-  unordered_map<ZXVertex*, size_t> table;
-  size_t cnt = 0;
-  for(auto v:neighbors){
-    table[v] = cnt;
-    cnt++;
-  }
-  for(auto v:frontier){
-    vector<unsigned char> storage = vector<unsigned char>(neighbors.size(), 0);
-    for(auto [vt,et]: v->getNeighbors()){
-      if(inNeighbors(vt, neighbors)){
-        //REVIEW - Assume no space in #qubit (0,2,3,4,5 is not allowed)
-        storage[table[vt]] = 1;
-        // in Neighbors
-      }
+/// @return
+bool M2::isIdentity() const {
+    // REVIEW - Only check one 1 in a row
+    for (const auto& row : _matrix) {
+        if (!row.isOneHot()) return false;
     }
-    _matrix.push_back(Row(1, storage));
-    printMatrix();
-  }
 
-  return true;
+    return true;
 }
 
-/// @brief Check vertex in frontier
-/// @param v 
-/// @param f 
-/// @return true if in, false if not
-bool M2::inNeighbors(ZXVertex* v, const ZXVertexList& n){
-  if(n.contains(v)) return true;
-  else return false;
+/**
+ * @brief Build matrix from ZX-graph (according to the given order)
+ *
+ * @param frontier
+ * @param neighbors
+ * @return true if successfully built,
+ * @return false if not
+ */
+bool M2::fromZXVertices(const ZXVertexList& frontier, const ZXVertexList& neighbors) {
+    if (frontier.size() != neighbors.size()) {
+        cout << "Numbers of elements in frontier and neighbors mismatch!" << endl;
+        return false;
+    }
+    // NOTE - assign row by calculating a Frontier's connecting status to Neighbors, e.g. 10010 = connect to qubit 0 and 3.
+
+    unordered_map<ZXVertex*, size_t> table;
+    size_t cnt = 0;
+    for (auto& v : neighbors) {
+        table[v] = cnt;
+        cnt++;
+    }
+    for (auto& v : frontier) {
+        vector<unsigned char> storage = vector<unsigned char>(neighbors.size(), 0);
+        for (auto& [vt, _] : v->getNeighbors()) {
+            if (neighbors.contains(vt)) {
+                // REVIEW - Assume no space in #qubit (0,2,3,4,5 is not allowed)
+                storage[table[vt]] = 1;
+                // in Neighbors
+            }
+        }
+        _matrix.push_back(Row(1, storage));
+        printMatrix();
+    }
+
+    return true;
 }
--- src/m2/m2.h
+++ src/m2/m2.h
@@ -9,13 +9,15 @@
 #ifndef M2_H
 #define M2_H
 
-#include "m2Def.h"
-#include <string>
-#include <vector>
-#include <iostream>
+#include <assert.h>
+
 #include <algorithm>
 #include <bitset>
-#include <assert.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "m2Def.h"
 #include "zxGraph.h"
 
 using namespace std;
@@ -26,48 +28,53 @@ class Row;
 //   Define classes
 //------------------------------------------------------------------------
 
-//REVIEW - Change if bit > 64
-class Row
-{
+// REVIEW - Change if bit > 64
+class Row {
 public:
-    Row(size_t id, const vector<unsigned char>& r): _id(id), _row(r) {}
+    Row(size_t id, const vector<unsigned char>& r) : _id(id), _row(r) {}
     ~Row() {}
-    const vector<unsigned char>& getRow()               { return _row; }
-    void setRow(vector<unsigned char> row)              { _row = row; }
-    size_t size()                                       { return _row.size(); }
-    bool isOneHot();
+    const vector<unsigned char>& getRow() const { return _row; }
+    void setRow(vector<unsigned char> row) { _row = row; }
+    size_t size() const { return _row.size(); }
+    bool isOneHot() const;
 
     void printRow() const;
 
-    Row &operator+=(const Row& rhs);
+    Row& operator+=(const Row& rhs);
+
+    unsigned char& operator[](const size_t& i) {
+        return _row[i];
+    }
+    const unsigned char& operator[](const size_t& i) const {
+        return _row[i];
+    }
+
 private:
     size_t _id;
     vector<unsigned char> _row;
 };
 
-class M2
-{
+class M2 {
 public:
     M2() {}
     ~M2() {}
 
-    //NOTE - Initializer
+    // NOTE - Initializer
     void defaultInit();
     bool fromZXVertices(const ZXVertexList&, const ZXVertexList&);
-    const vector<Row>& getMatrix()          { return _matrix; }
-    const vector<Oper>& getOpers()          { return _opStorage; }
-    const Row getRow(size_t r)              { return _matrix[r]; }
-    bool xorOper(size_t ctrl, size_t targ, bool track=false);
-    void gaussianElim(bool track=false);
-    bool isIdentity();
+    const vector<Row>& getMatrix() { return _matrix; }
+    const vector<Oper>& getOpers() { return _opStorage; }
+    const Row& getRow(size_t r) { return _matrix[r]; }
+    bool xorOper(size_t ctrl, size_t targ, bool track = false);
+    void gaussianElim(bool track = false);
+    bool isIdentity() const;
     void printMatrix() const;
     void printTrack() const;
 
 private:
-    size_t              _size;
-    vector<Row>         _matrix; 
-    vector<Oper>        _opStorage;
-    bool inNeighbors(ZXVertex*, const ZXVertexList&);
+    size_t _size;
+    vector<Row> _matrix;
+    vector<Oper> _opStorage;
 };
 
-#endif // M2_H
+#endif  // M2_H
