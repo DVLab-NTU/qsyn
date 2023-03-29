28328b798260fc0ddd477195ff573513bf85da67
JoshuaLau0220 <josh.lau2011@gmail.com>
2023-03-29 15:04:43 +0800

    :pencil2: forgot to comment out something

--- ref/overhaul_benchmark.sh
+++ ref/overhaul_benchmark.sh
@@ -3,10 +3,10 @@ doit() {
     ./qsyn -f $1 > $2 2>&1
 }
 export -f doit
-doit-mimalloc() {
-    LD_PRELOAD=libmimalloc.so ./qsyn -f $1 > ref/$2.log 2>&1
-}
-export -f doit-mimalloc
+# doit-mimalloc() {
+#     LD_PRELOAD=libmimalloc.so ./qsyn -f $1 > ref/$2.log 2>&1
+# }
+# export -f doit-mimalloc
 
 if [ $# -ne 1 ]; then
     echo "Error: wrong number of arguments!"
@@ -16,13 +16,6 @@ fi
 
 FOLDER=$1
 mkdir -p ref/${FOLDER}
-mkdir -p ref/${FOLDER}-mimalloc
-# parallel LD_PRELOAD=libmimalloc.so ./qsyn -f {} ::: ${files} > ref/${FOLDER}-mimalloc/{}.log 2>&1
+
 ls ref/freduce/*.dof | parallel doit {} ref/${FOLDER}/{/.}.log
-ls ref/freduce/*.dof | parallel doit-mimalloc {} ref/${FOLDER}-mimalloc/{/.}.log
-# ls ref/freduce/*.dof | parallel echo {} $(basename {}) # ./qsyn -f {} > ref/${FOLDER}-mimalloc/{}.log 2>&1
-# for f in ref/freduce/*; do
-#     echo ${f}
-#     # ./qsyn -f "ref/${file}.dof" > ref/${FOLDER}/${file}.log 2>&1
-#     # LD_PRELOAD=libmimalloc.so ./qsyn -f "ref/${file}.dof" > ref/${FOLDER}-mimalloc/${file}.log 2>&1
-# done
+# ls ref/freduce/*.dof | parallel doit-mimalloc {} ref/${FOLDER}-mimalloc/{/.}.log
