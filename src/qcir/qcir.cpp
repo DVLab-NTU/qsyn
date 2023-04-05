2efe5eb212ccb6ae9f895b58fb9f579fc07f68e9
arttr1521 <b08901173@g.ntu.edu.tw>
2023-04-05 16:55:25 +0800

    ♻️  Reconverse circuit->printGates()

--- src/qcir/qcir.cpp
+++ src/qcir/qcir.cpp
@@ -61,9 +61,10 @@ void QCir::printGates() {
     cout << "Listed by gate ID" << endl;
     for (size_t i = 0; i < _qgates.size(); i++) {
         _qgates[i]->printGate();
-        if(_qgates[i]->getType()==GateType::P){
-            cout << "ZPhase is  "<<_qgates[i]->getPhase() << endl;
-        }
+        //REVIEW - check the ZPhase
+        // if(_qgates[i]->getType()==GateType::P){
+        //     cout << "ZPhase is  "<<_qgates[i]->getPhase() << endl;
+        // }
     }
 }
 
