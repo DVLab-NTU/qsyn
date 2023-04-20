0fbe09e80a144225e9beacde33cd9638e82b42eb
chinyi0523 <james31423a@gmail.com>
2023-04-20 18:11:09 +0800

    🎨 Upgrade clang-format version to 16.0.3

--- src/optimizer/optimizerCmd.cpp
+++ src/optimizer/optimizerCmd.cpp
@@ -8,9 +8,9 @@
 
 #include "optimizerCmd.h"
 
-#include <cstddef>      // for size_t
-#include <iostream>     // for ostream
-#include <string>       // for string
+#include <cstddef>   // for size_t
+#include <iostream>  // for ostream
+#include <string>    // for string
 
 #include "optimizer.h"  // for Extractor
 #include "qcir.h"       // for QCir
