91e94076c3c40fd5780385ee7e9cd9c3d31a4192
arttr1521 <b08901173@g.ntu.edu.tw>
2023-04-24 17:07:33 +0800

    ➕ Create basic optimization files

---
+++ src/optimizer/basic_optimization.cpp
@@ -0,0 +1,8 @@
+/****************************************************************************
+  FileName     [ optimizer.h ]
+  PackageName  [ optimizer ]
+  Synopsis     [ Define class Optimizer structure ]
+  Author       [ Design Verification Lab ]
+  Copyright    [ Copyright(c) 2023 DVLab, GIEE, NTU, Taiwan ]
+****************************************************************************/
+
