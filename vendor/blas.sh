0b8257d131dc08b4f566ce58c9bf0c6bd5ac7e1c
chinyi0523 <james31423a@gmail.com>
2023-05-12 16:55:56 +0800

    📦 Fix vending installation path

--- vendor/blas.sh
+++ vendor/blas.sh
@@ -1,6 +1,7 @@
-ENGINE_NAME="BLAS-3.9.0"
-VENDOR_PATH="engine/$ENGINE_NAME"
-DOWNLOAD_PATH="http://www.netlib.org/blas/blas-3.9.0.tgz"
+ENGINE_NAME="BLAS-3.11.0"
+TIMESTAMP=$(date -I)
+VENDOR_PATH="/tmp/qsyn-configure-"$TIMESTAMP
+DOWNLOAD_PATH="http://www.netlib.org/blas/blas-3.11.0.tgz"
 JOB=16
 if [ $# -ne 1 ]; then 
     echo "[Error] Missing install path!"
@@ -8,9 +9,10 @@ if [ $# -ne 1 ]; then
 fi
 
 INSTALL_PATH=$1
-
-rm -rf ${VENDOR_PATH}
-cd engine
+echo $VENDOR_PATH
+mkdir -p ${VENDOR_PATH}
+rm -rf ${VENDOR_PATH}/${ENGINE_NAME}
+cd ${VENDOR_PATH}
 curl -L ${DOWNLOAD_PATH} --output ${ENGINE_NAME}.tgz
 tar -xf ${ENGINE_NAME}.tgz
 
--- vendor/lapack.sh
+++ vendor/lapack.sh
@@ -1,5 +1,6 @@
 ENGINE_NAME="lapack-3.10.1"
-VENDOR_PATH="engine/$ENGINE_NAME"
+TIMESTAMP=$(date -I)
+VENDOR_PATH="/tmp/qsyn-configure-"$TIMESTAMP
 DOWNLOAD_PATH="https://github.com/Reference-LAPACK/lapack/archive/v3.10.1.tar.gz"
 JOB=16
 if [ $# -ne 1 ]; then 
@@ -9,10 +10,11 @@ fi
 
 INSTALL_PATH=$1
 
-rm -rf ${VENDOR_PATH}
-cd engine
+mkdir -p ${VENDOR_PATH}
+rm -rf ${VENDOR_PATH}/${ENGINE_NAME}
+cd ${VENDOR_PATH}
 curl -L ${DOWNLOAD_PATH} --output ${ENGINE_NAME}.tar.gz
-tar -xf ${ENGINE_NAME}.tar.gz > /dev/null
+tar -xf $ENGINE_NAME.tar.gz > /dev/null
 
 cd ${ENGINE_NAME}
 cp make.inc.example make.inc
