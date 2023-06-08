417c2208a65c7e457e32f33a53ffdd64a67d2538
chinyi0523 <james31423a@gmail.com>
2023-06-08 21:25:24 +0800

    📦 Initialize level 2 functions (#395)

---
+++ src/extractor/greedyElim.cpp
@@ -0,0 +1,40 @@
+/****************************************************************************
+  FileName     [ greedyElim.cpp ]
+  PackageName  [ extractor ]
+  Synopsis     [ Define class Extractor member functions ]
+  Author       [ Design Verification Lab ]
+  Copyright    [ Copyright(c) 2023 DVLab, GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+#include <assert.h>  // for assert
+
+#include <memory>
+
+#include "extract.h"
+
+using namespace std;
+
+/**
+ * @brief
+ *
+ * @param matrix
+ * @param reversedSearch
+ * @return vector<size_t>
+ */
+vector<size_t> Extractor::findMinimalSums(M2& matrix, bool reversedSearch) {
+    vector<size_t> index;
+    // TODO -
+    return index;
+}
+
+/**
+ * @brief
+ *
+ * @param matrix
+ * @return vector<M2::Oper>
+ */
+vector<M2::Oper> Extractor::greedyReduction(M2& matrix) {
+    vector<M2::Oper> result;
+    // TODO -
+    return result;
+}
