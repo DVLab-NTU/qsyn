d13423e3d99b9bfd4b8a3309f11e47b1d267b083
chinyi0523 <james31423a@gmail.com>
2023-06-09 18:05:27 +0800

    📊 Update Opt2 ref and testcases (#395)

--- ref/extraction/dof_generator.py
+++ ref/extraction/dof_generator.py
@@ -6,7 +6,7 @@ filename = sys.argv[1]
 with open("countGates.dof", "w") as f:
     f.write("color off\n")
     f.write("qccr "+ sys.argv[1]+ '\n')
-    f.write("qccp -a\n")
+    f.write("qccp \n")
     # f.write("zxgsimp -fr\n")
     # f.write("zxgwrite zxgraph/"+sys.argv[1].split("/")[-1].split(".")[0]+".zx -c\n")
     f.write("qq -f\n")
