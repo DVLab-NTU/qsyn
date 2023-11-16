9a8127713d64245ce6e26eff416bda94c7787479
JoshuaLau0220 <josh.lau2011@gmail.com>
2023-11-17 00:25:41 +0800

    :sparkles: options to suppress dofile command echoing; -c flag

--- src/cli/cli_cmd.cpp
+++ src/cli/cli_cmd.cpp
@@ -236,16 +236,7 @@ Command source_cmd(CommandLineInterface& cli) {
             },
             [&cli](ArgumentParser const& parser) {
                 auto arguments = parser.get<std::vector<std::string>>("arguments");
-                if (!cli.add_variables_from_dofiles(parser.get<std::string>("file"), arguments)) {
-                    return CmdExecResult::error;
-                }
-
-                if (!cli.open_dofile(parser.get<std::string>("file"))) {
-                    spdlog::error("cannot open file \"{}\"!!", parser.get<std::string>("file"));
-                    return CmdExecResult::error;
-                }
-
-                return CmdExecResult::done;
+                return cli.source_dofile(parser.get<std::string>("file"), arguments, true);
             }};
 }
 
@@ -354,7 +345,6 @@ bool add_cli_common_cmds(dvlab::CommandLineInterface& cli) {
           cli.add_command(history_cmd(cli)) &&
           cli.add_command(help_cmd(cli)) &&
           cli.add_command(source_cmd(cli)) &&
-          cli.add_alias("dofile", "source") &&
           cli.add_command(usage_cmd()) &&
           cli.add_command(clear_cmd()) &&
           cli.add_command(logger_cmd()))) {
