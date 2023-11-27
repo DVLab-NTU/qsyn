78eb3334e75ae6b4e4e7919d277a165599cd4084
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-11-28 02:39:01 +0800

    fix docker test again

--- docker/test-entrypoint.sh
+++ docker/test-entrypoint.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 
-cmake -B ./build -S ./qsyn -DCOPY_EXECUTABLE=OFF
-cmake --build ./build --parallel "$(nproc)"
-./qsyn/scripts/RUN_TESTS --qsyn ./build/qsyn "$@"
+# in /app
+cmake -B /app/build -S /app/qsyn -DCOPY_EXECUTABLE=OFF
+cmake --build /app/build --parallel "$(nproc)"
+
+cd /app/qsyn || return # cd to /app/qsyn
+/app/qsyn/scripts/RUN_TESTS --qsyn /app/build/qsyn "$@"
