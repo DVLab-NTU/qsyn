35c5ff4be1d28d5ce7fa6e9513e051c4021f06f2
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-12-11 01:28:33 +0800

    feat: implement adapter for CaDiCal solver

---
+++ src/util/sat/cadical_solver.cpp
@@ -0,0 +1,45 @@
+/****************************************************************************
+  PackageName  [ util ]
+  Synopsis     [ Define the interface of SAT solver ]
+  Author       [ Design Verification Lab ]
+  Copyright    [ Copyright(c) 2023 DVLab, GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+#include "./cadical_solver.hpp"
+
+namespace dvlab::sat {
+
+CaDiCalSolver::~CaDiCalSolver() { delete _solver; }
+
+void CaDiCalSolver::reset() {
+    delete _solver;
+    _solver   = new CaDiCaL::Solver();
+    _num_vars = 0;
+}
+
+Variable CaDiCalSolver::new_var() { return ++_num_vars; }
+
+void CaDiCalSolver::add_clause(std::vector<Literal> const& clause) {
+    for (auto const& lit : clause) {
+        _solver->add(lit);
+    }
+    _solver->add(0);
+}
+
+void CaDiCalSolver::assume(Literal lit) {
+    _solver->assume(lit);
+}
+
+Result CaDiCalSolver::solve() {
+    switch (_solver->solve()) {
+        case 10:
+            return Result::SAT;
+        case 20:
+            return Result::UNSAT;
+        case 0:
+        default:
+            return Result::UNKNOWN;
+    }
+}
+
+}  // namespace dvlab::sat
