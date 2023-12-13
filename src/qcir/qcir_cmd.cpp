a1a716bfde4ae2a45d45b5f790cf5ae95232670f
jimmyb08901017 <jimmy312495@gmail.com>
2023-12-13 14:59:32 +0800

    feat: add qcir ph cmdline

--- src/qcir/qcir_cmd.cpp
+++ src/qcir/qcir_cmd.cpp
@@ -591,6 +591,35 @@ dvlab::Command qcir_adjoint_cmd(QCirMgr& qcir_mgr) {
             }};
 }
 
+dvlab::Command qcir_phase_polynomial_cmd(QCirMgr& qcir_mgr) {
+    return {"phase_polynomial",
+            [](ArgumentParser& parser) {
+                parser.description("perform phase polynomial optimizer");
+
+                // a=-1: unlimited ancilla
+                parser.add_argument<int>("-a", "--ancilla")
+                    .nargs(NArgsOption::optional)
+                    .default_value(-1)
+                    .help("the number of ancilla to be added (default=-1)");
+
+                parser.add_argument<std::string>("-resyn", "--resynthesis")
+                    .constraint(choices_allow_prefix({"C", "G"}))
+                    .default_value("G")
+                    .help("the resynethesis method chosen(C/G). If not specified, the default method i\ns gaussian elimination(G).");
+            },
+            [&](ArgumentParser const& parser) {
+                if (qcir_mgr.empty()) {
+                    spdlog::info("QCir list is empty now. Create a new one.");
+                    qcir_mgr.add(qcir_mgr.get_next_id());
+                }
+
+                // TODO
+                fmt::println("phase-polynomial {}", parser.get<std::string>("--resynthesis"));
+
+                return CmdExecResult::done;
+            }};
+}
+
 Command qcir_cmd(QCirMgr& qcir_mgr) {
     auto cmd = dvlab::utils::mgr_root_cmd(qcir_mgr);
 
@@ -610,6 +639,7 @@ Command qcir_cmd(QCirMgr& qcir_mgr) {
     cmd.add_subcommand(qcir_gate_cmd(qcir_mgr));
     cmd.add_subcommand(qcir_qubit_cmd(qcir_mgr));
     cmd.add_subcommand(qcir_optimize_cmd(qcir_mgr));
+    cmd.add_subcommand(qcir_phase_polynomial_cmd(qcir_mgr));
     return cmd;
 }
 
