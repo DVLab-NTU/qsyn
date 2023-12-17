5b00ea18cc02c840f1a2fd54d072d4b7090632d3
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-12-17 16:10:48 +0800

    feat: use fluent::NamedType for sat solver variable and literals

--- src/util/sat/sat_solver.hpp
+++ src/util/sat/sat_solver.hpp
@@ -6,16 +6,30 @@
 ****************************************************************************/
 
 #pragma once
+
+#include <NamedType/named_type.hpp>
+#include <NamedType/underlying_functionalities.hpp>
 #include <vector>
 
 namespace dvlab::sat {
 
-// Var is a positive integer
-typedef int Variable;
-// Lit is a signed integer
+// Variable is a positive integer
+using Variable = fluent::NamedType<int, struct VariableTag, fluent::Comparable, fluent::Hashable, fluent::PreIncrementable>;
+
+// Literal is a signed integer
 // The absolute value of Lit is the corresponding Var
 // Negation of Lit is represented by the negative sign
-typedef int Literal;
+class Literal : public fluent::NamedType<int, struct LiteralTag, fluent::Comparable, fluent::Hashable, fluent::UnarySubtractable> {
+private:
+    using _Literal = fluent::NamedType<int, struct LiteralTag, fluent::Comparable, fluent::Hashable, fluent::UnarySubtractable>;
+
+public:
+    Literal() = default;
+    Literal(int value) : _Literal(value) {}
+    Literal(Variable var, bool negate = false) : _Literal(negate ? var.get() : -var.get()) {}
+    Variable get_variable() const { return Variable(std::abs(get())); }
+    bool get_negated() const { return get() < 0; }
+};
 
 enum class Result {
     SAT,
