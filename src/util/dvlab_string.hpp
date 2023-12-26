2b7d3dc63c154c7f120041ac26c67ec3d57042d8
Mu-Te Joshua Lau <71618875+JoshuaLau0220@users.noreply.github.com>
2023-12-26 23:11:41 +0800

    :bug: fix #42

--- src/util/dvlab_string.hpp
+++ src/util/dvlab_string.hpp
@@ -227,15 +227,21 @@ template <typename T>
 requires std::floating_point<T>
 std::from_chars_result from_chars_wrapper(std::string_view str, T& val) {
     size_t pos = 0;
-    if constexpr (std::is_same_v<T, float>) {
-        val = std::stof(std::string{str}, &pos);
-        return {str.data() + pos, std::errc{}};
-    } else if constexpr (std::is_same_v<T, double>) {
-        val = std::stod(std::string{str}, &pos);
-        return {str.data() + pos, std::errc{}};
-    } else if constexpr (std::is_same_v<T, long double>) {
-        val = std::stold(std::string{str}, &pos);
-        return {str.data() + pos, std::errc{}};
+    try {
+        if constexpr (std::is_same_v<T, float>) {
+            val = std::stof(std::string{str}, &pos);
+            return {str.data() + pos, std::errc{}};
+        } else if constexpr (std::is_same_v<T, double>) {
+            val = std::stod(std::string{str}, &pos);
+            return {str.data() + pos, std::errc{}};
+        } else if constexpr (std::is_same_v<T, long double>) {
+            val = std::stold(std::string{str}, &pos);
+            return {str.data() + pos, std::errc{}};
+        }
+    } catch (std::invalid_argument const& e) {
+        return {str.data(), std::errc::invalid_argument};
+    } catch (std::out_of_range const& e) {
+        return {str.data(), std::errc::result_out_of_range};
     }
     DVLAB_UNREACHABLE("unsupported type");
 }
