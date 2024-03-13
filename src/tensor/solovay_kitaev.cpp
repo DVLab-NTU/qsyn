06c911f5cd8d8a3e3121a232e1d8c1980e62d783
annoyingcutie <jennydong1005@gmail.com>
2024-03-13 22:54:50 +0800

    Update remove_redundant_gate_function

--- src/tensor/solovay_kitaev.cpp
+++ src/tensor/solovay_kitaev.cpp
@@ -47,49 +47,52 @@ void SolovayKitaev::_dagger_matrices(std::vector<int>& sequence) {
 //  */
 void SolovayKitaev::_remove_redundant_gates(std::vector<int>& gate_sequence) {
    
-    size_t s = gate_sequence.size();
+    //size_t s = gate_sequence.size();
     size_t counter = 0;
      while (counter < gate_sequence.size() - 1) {
-        if (gate_sequence[counter] == 1 && gate_sequence[counter + 1] == -1 && counter + 1 < s) {
+        if (gate_sequence[counter] == 1 && gate_sequence[counter + 1] == -1 && counter + 1 < gate_sequence.size()) {
             gate_sequence.erase(gate_sequence.begin() + gsl::narrow<int>(counter), gate_sequence.begin() + gsl::narrow<int>(counter) + 2);
     
          } 
-        else if (gate_sequence[counter] == 0 && gate_sequence[counter + 1] == 0 && counter + 1 < s) {
+        else if (gate_sequence[counter] == 0 && gate_sequence[counter + 1] == 0 && counter + 1 < gate_sequence.size()) {
             gate_sequence.erase(gate_sequence.begin() + gsl::narrow<int>(counter), gate_sequence.begin() + gsl::narrow<int>(counter) + 2);
          }
-         else if (gate_sequence[counter] == 2 && gate_sequence[counter + 1] == -2 && counter + 1 < s) {
+         else if (gate_sequence[counter] == 2 && gate_sequence[counter + 1] == -2 && counter + 1 < gate_sequence.size()) {
             gate_sequence.erase(gate_sequence.begin() + gsl::narrow<int>(counter), gate_sequence.begin() + gsl::narrow<int>(counter) + 2);
          }
          else if (gate_sequence[counter] == 1 && gate_sequence[counter + 1] == 1 && 
                   gate_sequence[counter + 2] == 1 && gate_sequence[counter + 3] == 1 && 
                   gate_sequence[counter + 4] == 1 && gate_sequence[counter + 5] == 1 && 
-                  gate_sequence[counter + 6] == 1 && gate_sequence[counter + 7] && counter + 7 < s) {
+                  gate_sequence[counter + 6] == 1 && gate_sequence[counter + 7] && counter + 7 < gate_sequence.size()) {
              gate_sequence.erase(gate_sequence.begin() + gsl::narrow<int>(counter), gate_sequence.begin() + gsl::narrow<int>(counter) + 8);
         } 
         else if (gate_sequence[counter] == -1 && gate_sequence[counter + 1] == -1 && 
                   gate_sequence[counter + 2] == -1 && gate_sequence[counter + 3] == -1 && 
                   gate_sequence[counter + 4] == -1 && gate_sequence[counter + 5] == -1 && 
-                  gate_sequence[counter + 6] == -1 && gate_sequence[counter + 7] && counter + 7 < s) {
+                  gate_sequence[counter + 6] == -1 && gate_sequence[counter + 7] && counter + 7 < gate_sequence.size()) {
             gate_sequence.erase(gate_sequence.begin() + gsl::narrow<int>(counter), gate_sequence.begin() + gsl::narrow<int>(counter) + 8);
         }
-        else if (gate_sequence[counter] == -1 && gate_sequence[counter + 1] == -1 && counter + 1 < s) {
-             
+        else if (gate_sequence[counter] == -1 && gate_sequence[counter + 1] == -1 && counter + 1 < gate_sequence.size()) {
+             gate_sequence.erase(gate_sequence.begin() + gsl::narrow<int>(counter), gate_sequence.begin() + gsl::narrow<int>(counter) + 2);
              gate_sequence.insert(gate_sequence.begin() + gsl::narrow<int>(counter) -1, -2);
              counter++;
         } 
-        else if (gate_sequence[counter] == 1 && gate_sequence[counter + 1] == 1 && counter + 1 < s) {
+        else if (gate_sequence[counter] == 1 && gate_sequence[counter + 1] == 1 && counter + 1 < gate_sequence.size()) {
+            gate_sequence.erase(gate_sequence.begin() + gsl::narrow<int>(counter), gate_sequence.begin() + gsl::narrow<int>(counter) + 2);
              gate_sequence.insert(gate_sequence.begin() + gsl::narrow<int>(counter) -1, 2);
              counter++;
         }
         else if (gate_sequence[counter] == 1 && gate_sequence[counter + 1] == 1 && 
                  gate_sequence[counter + 2] == 1 && gate_sequence[counter + 3] == 1 
-                 && counter + 3 < s) {
+                 && counter + 3 < gate_sequence.size()) {
+             gate_sequence.erase(gate_sequence.begin() + gsl::narrow<int>(counter), gate_sequence.begin() + gsl::narrow<int>(counter) + 4);
              gate_sequence.insert(gate_sequence.begin() + gsl::narrow<int>(counter) -1, 4);
              counter++;;
         }
         else if (gate_sequence[counter] == 0 && gate_sequence[counter + 1] == 2 && 
                  gate_sequence[counter + 2] == 2 && gate_sequence[counter + 3] == 0 
-                 && counter + 3 < s) {
+                 && counter + 3 < gate_sequence.size()) {
+             gate_sequence.erase(gate_sequence.begin() + gsl::narrow<int>(counter), gate_sequence.begin() + gsl::narrow<int>(counter) + 4);
              gate_sequence.insert(gate_sequence.begin() + gsl::narrow<int>(counter) -1, -4);
              counter++;
         }    
